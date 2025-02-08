2e487ea8f1ee99b098db287ab48d17e591301f24
karan <kr14nov@outlook.com>
2025-02-08 17:17:39 +0530

    Installed glfw3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-#include "GLFW"
+#include <GLFW/glfw3.h>
 
 int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW3" << std::endl;
+        return EXIT_FAILURE;
+    }
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
